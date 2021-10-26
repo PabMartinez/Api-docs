36769da201c4ebfd2c24d75735245a821eb2cf5b
alfonso.lozana <alfonso.lozana@freightol.com>
2021-10-26 13:06:04 +0200

    Add airport and postal code doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Freightol'
+copyright = '2021, Freightol'
+author = 'Freightol'
 
 release = '0.1'
 version = '0.1.0'
